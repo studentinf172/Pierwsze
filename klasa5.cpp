b5b1723c5c3e75c14a0824de71def04071be7903
michal <mielusmichal@gmail.com>
2015-12-01 08:41:56 +0100

    Piąty

---
+++ klasa5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void max()
+{
+    int a = 0;
+    int liczba = 0;
+    int max = 0;
+
+    for(int i = 0; i < 10; i ++)
+    {
+        cout << Podaj liczbe <<endl;
+        cin >> liczba;
+
+        if(i == 0) max = liczba;
+        else
+        {
+            if(max < liczba)) max = liczba;
+        }
+    }
+}
+int main()
+{
+    cout << "Hello";
+    cout << "Kod";
+    for(int i = 0; i < 10; i ++)
+    {
+        cout << "Heuehe";
+    }
+
+    int a = 20;
+    while (a)
+    {
+        cout << a <<endl;
+        --a;
+    }
+
+
+    return 0;
+}
