d9154c55a831641c2fd3f2be9c5238d61649cf48
michal <mielusmichal@gmail.com>
2015-12-01 08:40:10 +0100

    Pierwszy komit

--- klasa1.cpp
+++ klasa1.cpp
@@ -1,11 +1,40 @@
-#include "klasa1.h"
+#include <iostream>
+using namespace std;
 
-klasa1::klasa1()
+void max()
 {
-    //ctor
+    int a = 0;
+    int liczba = 0;
+    int max = 0;
+
+    for(int i = 0; i < 10; i ++)
+    {
+        cout << Podaj liczbe <<endl;
+        cin >> liczba;
+
+        if(i == 0) max = liczba;
+        else
+        {
+            if(max < liczba)) max = liczba;
+        }
+    }
 }
-
-klasa1::~klasa1()
+int main()
 {
-    //dtor
+    cout << "Hello";
+    cout << "Kod";
+    for(int i = 0; i < 10; i ++)
+    {
+        cout << "Heuehe";
+    }
+
+    int a = 20;
+    while (a)
+    {
+        cout << a <<endl;
+        --a;
+    }
+
+
+    return 0;
 }
